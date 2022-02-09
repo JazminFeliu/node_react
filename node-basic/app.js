a0db1d09534e206b424954c454076f6857839542
JazminFeliu <jazminfeliu@gmail.com>
2022-02-09 14:56:00 -0300

    first commit

---
+++ node-basic/app.js
@@ -0,0 +1,12 @@
+console.log("Hello from node js");
+
+//learn about functions
+
+function sum(a,b){
+    return a + b;
+}
+
+const total = sum(30000, 5678);
+
+console.log(sum(2,4));
+console.log("El total es: ",total);
