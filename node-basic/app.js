11f2e14c89795a1de42f9e33076d2ff82d7840d1
JazminFeliu <jazminfeliu@gmail.com>
2022-02-09 15:13:03 -0300

    subo actualizacion de readme

--- node-basic/app.js
+++ node-basic/app.js
@@ -1,12 +1,12 @@
-console.log("Hello from node js");
+//console.log("Hello from node js");
 
 //learn about functions
 
-function sum(a,b){
-    return a + b;
-}
+// const helpers = require("./helpers");
 
-const total = sum(30000, 5678);
+// const total = helpers.sum(30000, 5678);
 
-console.log(sum(2,4));
-console.log("El total es: ",total);
+// console.log(helpers.sum(2,4));
+// console.log("El total es: ",total);
+
+console.log("PROCESS: ",process);
---
+++ node-basic/helpers.js
@@ -0,0 +1,14 @@
+
+function sum(a,b){
+    return a + b;
+}
+
+module.exports = {
+    sum
+}
+
+//uso el module exports para hexportar un metodo
+//requerido por otro modulo
+
+
+console.log("PROCESS: ",process);
