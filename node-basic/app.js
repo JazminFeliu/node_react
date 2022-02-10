5089c30f7a7fd3a8ae81b09d034187c792c98efe
JazminFeliu <jazminfeliu@gmail.com>
2022-02-10 10:37:30 -0300

    uso de modulos basicos

--- node-basic/app.js
+++ node-basic/app.js
@@ -2,11 +2,24 @@
 
 //learn about functions
 
-// const helpers = require("./helpers");
+const helpers = require("./helpers");
+const http = require('http')
 
-// const total = helpers.sum(30000, 5678);
+const server = http.createServer((req, res) => {
+    res.end("hello world from node js")
+});
 
-// console.log(helpers.sum(2,4));
-// console.log("El total es: ",total);
+server.listen(8080);
 
-console.log("PROCESS: ",process);
+const total = helpers.sum(30000, 5678);
+const a = 12;
+const b = 3;
+console.log(helpers.sum(a,b));
+console.log(helpers.resta(a,b));
+console.log(helpers.divi(a,b));
+console.log(helpers.mul(a,b));
+
+console.log(helpers.sum(2,4));
+console.log("El total es: ",total);
+
+//console.log("PROCESS: ",process);
--- node-basic/helpers.js
+++ node-basic/helpers.js
@@ -1,14 +1,25 @@
 
-function sum(a,b){
-    return a + b;
-}
+// function sum(a,b){
+//     return a + b;
+// }
 
-module.exports = {
-    sum
-}
+// const sum = (a, b) => {
+//     return a + b;
+// }
 
-//uso el module exports para hexportar un metodo
+// const sum = (a, b) => a + b;
+
+// module.exports = {
+//     sum
+// }
+
+exports.sum = (a,b) => a + b;
+exports.resta = (a,b) => a - b;
+exports.divi = (a,b) => a / b;
+exports.mul = (a,b) => a * b;
+
+//uso el module exports para exportar un metodo
 //requerido por otro modulo
 
 
-console.log("PROCESS: ",process);
+//console.log("PROCESS: ",process);
