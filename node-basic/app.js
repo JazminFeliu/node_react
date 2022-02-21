8856f1caf2f1d4b826e372b5cf04581609769471
JazminFeliu <jazminfeliu@gmail.com>
2022-02-21 15:57:34 -0300

    subo secretos de nodeJS

--- node-basic/app.js
+++ node-basic/app.js
@@ -14,14 +14,14 @@ const fs = require("fs")
 const fileName = "target.txt"
 // fs.watch(fileName, () => console.log("File changed!"));
 
-const data = fs.readFileSync(fileName)
-console.log(data.toString());
+const errHandler = err => console.log(err);
 
-// fs.readFile(fileName, (err, data) => {
-//     if (err){
-//         console.log(err)
-//     }
-//     console.log(data.toString());
-// });
+const dataHandler = data => console.log(data.toString());
+
+fs.readFile(fileName, (err, data) => {
+    if (err) errHandler(err);
+
+    dataHandler(data);
+});
 
 console.log("Node js async programming...?");
