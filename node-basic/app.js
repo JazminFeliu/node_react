f527b7ebec7e593f6a8f4a89e623f3ad2073cfbc
JazminFeliu <jazminfeliu@gmail.com>
2022-02-21 15:44:08 -0300

    ejemplo de programacion sincrona

--- node-basic/app.js
+++ node-basic/app.js
@@ -14,11 +14,14 @@ const fs = require("fs")
 const fileName = "target.txt"
 // fs.watch(fileName, () => console.log("File changed!"));
 
-fs.readFile(fileName, (err, data) => {
-    if (err){
-        console.log(err)
-    }
-    console.log(data.toString());
-});
+const data = fs.readFileSync(fileName)
+console.log(data.toString());
+
+// fs.readFile(fileName, (err, data) => {
+//     if (err){
+//         console.log(err)
+//     }
+//     console.log(data.toString());
+// });
 
 console.log("Node js async programming...?");
