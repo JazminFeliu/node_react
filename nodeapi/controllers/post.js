7a1c622ea3596f89d5cb690e3c2f8a9d1893a630
JazminFeliu <jazminfeliu@gmail.com>
2022-02-24 14:23:41 -0300

    err val manual y  con express-validator

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -1,8 +1,25 @@
+const Post = require("../models/post")
 exports.getPosts = (req, res) => {
     res.json({
         posts: [
             {title : "First post"},{title: "Second post"}
         ]
+    });
+};
+
+exports.createPost = (req, res) => {
+    const post = new Post(req.body)
+    // console.log("CREATING POST: ", req.body);
+    // post.save((err, result) => {
+    //     res.status(200).json({
+    //         post: result
+    //     });
+    // } );
+    post.save()
+    .then(result => {
+        res.status(200).json({
+            post:result
+        });
     });
 };
 
