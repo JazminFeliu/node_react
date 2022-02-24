82f92c16dfdab19e81bec249d704a007935ef9c7
JazminFeliu <jazminfeliu@gmail.com>
2022-02-24 11:52:52 -0300

    subo uso mongoose, dotenv

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,6 +1,18 @@
 const express = require("express")
 const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const dotenv = require("dotenv")
+dotenv.config();
+
+
+//db
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB connected!"))
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connetion error: ${err.menssage}`);
+});
 
 //bring in routes
 const postRoutes = require("./routes/post");
@@ -11,7 +23,7 @@ app.use(morgan("dev"));
 
 app.use("/", postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`A NodeJS API is listening on port: ${port}`); 
